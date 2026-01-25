7bb53cc27c239d891e26ce3a1f9b2d7aef06c194
henish47 <henishpatel47@gmail.com>
2026-01-25 15:02:49 +0530

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
-import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <App />
-      <SpeedInsights/>
+      
       <Analytics/>
     </HelmetProvider>
   </StrictMode>,
